f4f2d38c6f74e5a2cefb09338beb85e661c01b76
Andreas Smolenko <a.smolenko@fz-juelich.de>
2023-10-14 12:22:45 +0200

    Erstelle die .xml und die .xsd Datei von Neuem bei der Ausführung des Programms

--- XSDValidate/XSDSchemaValidierung/Program.cs
+++ XSDValidate/XSDSchemaValidierung/Program.cs
@@ -4,6 +4,23 @@ using System.Xml;
 using System.IO;
 using System.Xml.Schema;
 using System.Xml.Linq;
+using System.Text;
+
+var fileName = "lifiBeispiel.xsd";
+if (File.Exists(fileName))
+{
+    File.Delete(fileName);
+}
+string fileContent = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<xs:schema attributeFormDefault=\"unqualified\" elementFormDefault=\"qualified\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\r\n  <xs:element name=\"verkehrInformation\">\r\n    <xs:complexType>\r\n      <xs:sequence>\r\n        <xs:element name=\"strasse\">\r\n          <xs:complexType>\r\n            <xs:sequence>\r\n              <xs:element name=\"name\" type=\"xs:string\" />\r\n              <xs:element name=\"verkehrStatus\" type=\"xs:string\" />\r\n              <xs:element name=\"geschwindigkeitsbegrenzung\" type=\"xs:unsignedByte\" />\r\n              <xs:element name=\"strasseZustand\" type=\"xs:string\" />\r\n            </xs:sequence>\r\n            <xs:attribute name=\"id\" type=\"xs:unsignedShort\" use=\"required\" />\r\n          </xs:complexType>\r\n        </xs:element>\r\n      </xs:sequence>\r\n    </xs:complexType>\r\n  </xs:element>\r\n</xs:schema>";
+File.WriteAllText(fileName, fileContent);
+
+fileName = "lifiBeispiel.xml";
+if (File.Exists(fileName))
+{
+    File.Delete(fileName);
+}
+fileContent = "<verkehrInformation>\r\n  <strasse id=\"7123\">\r\n    <name>Hauptstrasse</name>\r\n    <verkehrStatus>moderat</verkehrStatus>\r\n    <geschwindigkeitsbegrenzung>50</geschwindigkeitsbegrenzung>\r\n    <strasseZustand>einwandfrei</strasseZustand>\r\n  </strasse>\r\n</verkehrInformation>\r\n";
+File.WriteAllText(fileName, fileContent);
 
 string currDir = Directory.GetCurrentDirectory();
 string xsdFilePath = "" + currDir + "\\lifiBeispiel.xsd";
