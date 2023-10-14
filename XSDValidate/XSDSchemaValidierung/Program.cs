f8db2607322021a1996384506fcbea4fc7ea8085
Andreas Smolenko <a.smolenko@fz-juelich.de>
2023-10-14 12:01:14 +0200

    Add C# Sample Project to Validate xml Files

---
+++ XSDValidate/XSDSchemaValidierung/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using System.IO;
+using System.Xml.Schema;
+using System.Xml.Linq;
+
+string currDir = Directory.GetCurrentDirectory();
+string xsdFilePath = "" + currDir + "\\lifiBeispiel.xsd";
+string xmlFilePath = "" + currDir + "\\lifiBeispiel.xml";
+
+XmlSchemaSet schema = new XmlSchemaSet();
+schema.Add(null, xsdFilePath);
+
+XmlReader rd = XmlReader.Create(xmlFilePath);
+XDocument doc = XDocument.Load(rd);
+doc.Validate(schema, (o, e) =>
+{
+    Console.WriteLine("{0}", e.Message);
+});
